dd39a6bf4ca86977c90f47cab4718033fdf220b1
Yume Takeuchi <yume.take07@gmail.com>
2023-05-24 20:41:46 +0900

    change text

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    {{ __('You are logged in!') }}
+                    {{ __('YOU ARE AAAAAAAAA!') }}
                 </div>
             </div>
         </div>
