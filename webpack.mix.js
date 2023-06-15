988ded6d7543c3e42c34a1141b21b27948d998ea
Aimi Fujimura <gogoaimi2@yahoo.co.jp>
2023-06-15 21:29:08 +0900

    removed line 15

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,7 +12,6 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-    // .sass('resources/sass/app.scss', 'public/css')
     .sass('resources/scss/button.scss', 'public/css')
     .sass('resources/scss/form.scss', 'public/css')
     .sass('resources/scss/style.scss', 'public/css')
