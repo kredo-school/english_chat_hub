c57e35a9d404c8d14206ccaeffceeea88614e118
JuriMino <juri.mino1023@gmail.com>
2023-06-15 22:22:35 +0900

    try to resolve the conflct

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,7 +11,7 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
+ mix.js('resources/js/app.js', 'public/js')
     .sass('resources/scss/button.scss', 'public/css')
     .sass('resources/scss/form.scss', 'public/css')
     .sass('resources/scss/style.scss', 'public/css')
@@ -19,6 +19,8 @@ mix.js('resources/js/app.js', 'public/js')
     .sass('resources/scss/contact-us.scss', 'public/css')
     .sass('resources/scss/modal.scss', 'public/css')
     .sass('resources/scss/faq.scss', 'public/css')
-    .sass('resources/scss/admin-style.scss', 'public/css')
 
+
+
+    .sass('resources/scss/admin-style.scss', 'public/css')
     .sourceMaps();
