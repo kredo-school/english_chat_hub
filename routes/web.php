3d07ad43a2a1741f3ab637a487a2780e9a585b22
JuriMino <juri.mino1023@gmail.com>
2023-06-17 11:52:47 +0900

    create inbox/index.blade.php

--- routes/web.php
+++ routes/web.php
@@ -117,6 +117,9 @@ Route::group(['prefix' => 'events' , 'as' => 'events.'], function(){
                 Route::delete('/{category}/delete', [CategoriesController::class, 'delete'])->name('delete');
             });
         });
+
+        Route::group(['prefix' => 'inbox', 'as' => 'inbox.'],function(){
+            Route::get('/',[AdminController::class,'showInbox'])->name('show');
+        });
     });
-});
 });
