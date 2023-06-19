b4c22269a4ff61cf534aaa3cd7497b9da7b9ba42
JuriMino <juri.mino1023@gmail.com>
2023-06-19 19:50:29 +0900

    Creating inbox/modal/message-detail.blade.php

--- resources/views/admin/inbox/modal/message-detail.blade.php
+++ resources/views/admin/inbox/modal/message-detail.blade.php
@@ -1,11 +1,15 @@
 <div class="modal fade" id="message-detail-{{ $message->id }}">
   <div class="modal-dialog">
-      <div class="modal-content">
+      <div class="modal-content py-5 px-2">
           <div class="modal-body p-3 text-start">
-            <h1 class="h3">{{ $message->name }}</h1>
-             <p class="h4">Title: {{ $message->title }}</p>
-             <p class="h4">Subtitle: {{ $message->subtitle->name }}</p>
-             <p class="h4">From: {{ $message->email }}</p>
+            <p class="mb-0">{{ $message->created_at }}</p>
+            <div class="fs-5">
+              <p class="d-inline">Name: {{ $message->name }}</h1>
+              <p class="d-inline ms-5">From: {{ $message->email }}</p>
+            </div>
+             <p class="h3 fw-bolder my-1">Title: {{ $message->title }}</p>
+             <p class="h5 fw-bold">Subtitle: {{ $message->subtitle->name }}</p>
+             
              <hr>
              <div class="message">
               {{ $message->content}}
