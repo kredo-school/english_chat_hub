2dc90f1e8e36782c1166ed6e98793530671af425
KaiIto1710 <norman.xa@outlook.jp>
2023-06-21 19:29:55 +0900

    1. fix database. Modify start_at column in meetings table (type change 'int' -> 'time') 2. fix model, contorller and blade files related with the db changing

--- resources/views/admin/chatrooms/components/meetingList.blade.php
+++ resources/views/admin/chatrooms/components/meetingList.blade.php
@@ -8,7 +8,7 @@
                         <td style="width: 50px">{{ $loop->index + 1 }}</td>
                         <td class="text-start">{{ $meeting->title }}</td>
                         <td>{{ $meeting->user->user_name }}</td>
-                        <td>{{ $meeting->date }} {{ $meeting->start_at }} : 00~</td>
+                        <td>{{ $meeting->date }} {{ date('G:i', strtotime($meeting->start_at)) }}~</td>
                         <td>{{ request()->is('*/rooms/*') ? $meeting->category()->withTrashed()->first()->name : $meeting->room()->withTrashed()->first()->name }}</td>
                         <td style="width: 100px">
                             @if ($meeting->deleted_at)
