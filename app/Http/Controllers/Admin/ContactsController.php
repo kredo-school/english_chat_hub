7bf683d04daeb47ca613ba4b54f66427611e6571
JuriMino <juri.mino1023@gmail.com>
2023-06-30 22:32:31 +0900

    The controller was split and rebuilt for clarity. Also I deleted AdminController because it's useless.

---
+++ app/Http/Controllers/Admin/ContactsController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Contact;
+
+class ContactsController extends Controller
+{
+    // Inbox
+    public function show()
+    {
+        $all_messages = Contact::all();
+
+        return view('admin.inbox.index')->with('all_messages',$all_messages);
+    }
+    public function updateStatus(Request $request, Contact $message)
+    {
+        $message->status_id = $request->input('status');
+        $message->save();
+    
+        return redirect()->back();
+    }
+}
--- resources/views/admin/events/edit.blade.php
+++ resources/views/admin/events/edit.blade.php
@@ -8,7 +8,7 @@
         <div class="row justify-content-center">
             <div class="col-6 event-form ">
                 <h1>Edit Event</h1>
-                <form action="{{ route('admin.updateEvent',$event->id)}}" method="post" enctype="multipart/form-data">
+                <form action="{{ route('admin.events.update',$event->id)}}" method="post" enctype="multipart/form-data">
                     @csrf
                     @method('PATCH')
                     <div class="form-group my-4">
@@ -59,7 +59,7 @@
                     </div>
 
                     <button type="submit" class="my-5 btn px-5 ms-3 button btn-gray float-end">Create</button>
-                    <a href="{{route('admin.showEvents')}}" type="button" class="my-5 btn px-5 button btn-gray float-end">Back</a>
+                    <a href="{{route('admin.events.show')}}" type="button" class="my-5 btn px-5 button btn-gray float-end">Back</a>
 
                 </form>
             </div>
