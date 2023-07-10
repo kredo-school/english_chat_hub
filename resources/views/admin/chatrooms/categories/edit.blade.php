bb401da510cb8f0565595abdcb09a9bfb89b2a7c
KaiIto1710 <norman.xa@outlook.jp>
2023-07-10 19:38:26 +0900

    move jquery cdn script tag into layout page.

--- resources/views/admin/chatrooms/categories/edit.blade.php
+++ resources/views/admin/chatrooms/categories/edit.blade.php
@@ -80,8 +80,6 @@
 @endsection
 
 @section('scripts')
-    <script src="https://code.jquery.com/jquery-3.7.0.min.js"
-        integrity="sha256-2Pmvv0kuTBOenSvLm6bvfBSSHrUJ+3A7x6P5Ebd07/g=" crossorigin="anonymous"></script>
     <script>
         $(document).ready(function() {
             $('input[type="color"]').change(function() {
